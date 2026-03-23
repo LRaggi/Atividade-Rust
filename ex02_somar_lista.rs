ed6e729b2e209b8317d28f659c76f858039f70a5
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 22:47:11 -0300

    Document algorithm complexity and logic
    Added algorithm exercise description and complexity analysis.

--- ex02_somar_lista.rs
+++ ex02_somar_lista.rs
@@ -1,1 +1,6 @@
-
+## Exercício N — Nome do Algoritmo 
+**Complexidade:** O(n) 
+**Lógica do algoritmo:** 
+O algoritmo soma a quantidade de itens em uma lista.
+**Justificativa da complexidade:** 
+Não há nenhum loop, o algoritmo só executa uma única vez. O tempo de N depende do tamanho da lista.
