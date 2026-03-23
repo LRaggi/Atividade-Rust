0ff2a09c788a25301b7e4f3c0adee376fd659809
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 23:31:10 -0300

    Fix typo in merge sort algorithm explanation

--- ex10_merge_sort.rs
+++ ex10_merge_sort.rs
@@ -3,4 +3,4 @@
 **Lógica do algoritmo:** 
 O algoritmo divide o problema (lista) em partes menores, resolve-as individualmente, e depois as junta para ter o resultado final.
 **Justificativa da complexidade:** 
-Não há loop, uma vez que o algoritmo resolve uma única vez o porblema. O tempo de N varia pelo tamanho do problema (lista). 
+Não há loop, uma vez que o algoritmo resolve uma única vez o problema. O tempo de N varia pelo tamanho do problema (lista). 
