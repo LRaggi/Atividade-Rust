d4978b0db290206bc9cb6de295b3c993d8f0d2af
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 23:17:58 -0300

    Document Fibonacci recursive algorithm details
    Add explanation and complexity analysis for Fibonacci recursion

--- ex07_fibonacci_recursivo.rs
+++ ex07_fibonacci_recursivo.rs
@@ -1,1 +1,6 @@
-
+## Exercício 7 — Fibonacci Recursivo
+**Complexidade:** O(n) 
+**Lógica do algoritmo:** 
+O algoritmo realiza uma sequência de fibonacci recursiva, onde cada número é a soma dos dois anteriores. 
+**Justificativa da complexidade:** 
+Não há nenhum loop porque não há uma declaração de repetição, o comando é executado uma única vez. O tempo de N é indeterminado porse tratar de uma sequência tendeciosamente infinita.
