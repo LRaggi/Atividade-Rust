b959b36297da3420a00088c69037fd02b1b4386a
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 23:22:53 -0300

    Document bubble sort algorithm in ex08_ordenacao_bolha.rs
    Add comments explaining bubble sort algorithm and its complexity

--- ex08_ordenacao_bolha.rs
+++ ex08_ordenacao_bolha.rs
@@ -1,1 +1,6 @@
-
+## Exercício 8 — Ordenação Bolha 
+**Complexidade:** O(n²) 
+**Lógica do algoritmo:** 
+O algoritmo, da esquerda para a direita, examina cada conjunto de elementos e troca suas posições se estiverem erradas. 
+**Justificativa da complexidade:** 
+ Há N loops, pois depende do tamanho do escopo e da quantidade de trocas necessárias. O tempo de N varia de acordo com o tamanho do escopo.
