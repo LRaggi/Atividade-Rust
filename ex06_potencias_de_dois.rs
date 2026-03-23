dffca1308437281840a1138ecaae0eb73d2455b4
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 23:02:43 -0300

    Document algorithm logic and complexity in ex06_potencias_de_dois.rs
    Add comments explaining the algorithm and its complexity.

--- ex06_potencias_de_dois.rs
+++ ex06_potencias_de_dois.rs
@@ -1,1 +1,6 @@
-
+## Exercício 6 — Potências de Dois 
+**Complexidade:** O(n) 
+**Lógica do algoritmo:** 
+O algoritmo imprime os números que são potência de dois.
+**Justificativa da complexidade:** 
+Não há nenhum loop, uma vez que ele executa um único comando uma única vez. O tempo de N é indefinido e não termina, visto que ele busca todos os números da potência do número 2.
