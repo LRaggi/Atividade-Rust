5534cc3e789cd70c425aefd0b8dc84b28d0b071a
LRaggi <164022994+LRaggi@users.noreply.github.com>
2026-03-22 23:30:36 -0300

    Document Merge Sort algorithm and complexity
    Added comments explaining the Merge Sort algorithm and its complexity.

--- ex10_merge_sort.rs
+++ ex10_merge_sort.rs
@@ -1,1 +1,6 @@
-
+## Exercício 10 — Merge Sort 
+**Complexidade:** O(n) 
+**Lógica do algoritmo:** 
+O algoritmo divide o problema (lista) em partes menores, resolve-as individualmente, e depois as junta para ter o resultado final.
+**Justificativa da complexidade:** 
+Não há loop, uma vez que o algoritmo resolve uma única vez o porblema. O tempo de N varia pelo tamanho do problema (lista). 
